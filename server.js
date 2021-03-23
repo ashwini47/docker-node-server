93f18d6af20771bdb4dfd097b5436aa16b99f8bd
ashwini47 <ashwini47@gmail.com>
2021-03-23 15:50:58 -0500

    created apis routes

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var http = require('http');
-var handleRequest = function (request, response) {
-  response.writeHead(200);
-  
-  response.end('hellow world uupdate ver 4');
-  
-};
+var handleRequest = function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.writeHead(200);
+  var url = req.url;
+ if(url ==='/about'){
+  res.write(JSON.stringify({ message: "About Page" ,localTime:(new Date().toLocaleString())}));
+    res.end(); //end the response
+ }else if(url ==='/contact'){
+  res.write(JSON.stringify({ message: "Contact Page" ,localTime:(new Date().toLocaleString())}));
+    res.end(); //end the response
+ }else{
+    res.write(JSON.stringify({ message: "Hello World" ,localTime:(new Date().toLocaleString())}));
+    //res.write(JSON.stringify(new Date().toLocaleDateString())); 
+    res.end(); //end the response
+ }
+}
+
 var www = http.createServer(handleRequest);
 www.listen(8080);
